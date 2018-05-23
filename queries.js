5f36ae261be2456936fda5bae85fb3855a60e5cf
MDjavaheri <michael.djavaheri@mail.yu.edu>
2018-05-23 18:30:55 +0300

    global regex, query function, clean up readme

--- queries.js
+++ queries.js
@@ -1,25 +1,33 @@
 // Setup
-var hebrew = /[\u0590-\u05fe]/;
-var rama = /הגה [^:]*:/;
-var brackets = /\[[^\]]*\]/;
-var parens = /\([^\)]*\)/;
+var hebrew = /[\u0590-\u05fe]/g;
+var rama = /הגה [^:]*:/g;
+var brackets = /\[[^\]]*\]/g;
+var parens = /\([^\)]*\)/g;
 
 // Shulchan Aruch Queries
 var stamVeYesh;
 var yeshOmrimVeYeshOmrim;
 var yeshOmrimVeYeshOmer;
 var yeshOmrimVeYeshCholek;
-var yeshYachidOpening = /^יש מי שאומר/;
-var yeshYachid = /יש מי שאומר/;
-var yeshMattirOpening = /^יש מי שמתיר/;
-var yeshMattir = /יש מי שמתיר/;
-var yeshNohegYachidOpening = /^יש מי שנוהג/;
-var yeshNohegYachid = /יש מי שנוהג/;
-var yeshNohagimOpening = /^יש נוהגי[םן]/
-var yeshNohagim = /יש נוהגי[םן]/
-var yeshCholkim = /יש חולקי[םן]/
-var yeshCholek = /יש מי שחולק/
+
+var veYeshOmrimStart = /^ו(י"א|יש אומרים)/g;
+var yeshYachidOpening = /^יש מי שאומר/g;
+var veYeshOmerStart = /^ו(יש מי שאומר)/g;
+var yeshYachid = /יש מי שאומר/g;
+var yeshMattirOpening = /^יש מי שמתיר/g;
+var yeshMattir = /יש מי שמתיר/g;
+var yeshNohegYachidOpening = /^יש מי שנוהג/g;
+var yeshNohegYachid = /יש מי שנוהג/g;
+var yeshNohagimOpening = /^יש נוהגי[םן]/g;
+var yeshNohagim = /יש נוהגי[םן]/g;
+var yeshCholkim = /יש חולקי[םן]/g;
+var yeshCholek = /יש מי שחולק/g;
 var stamVeYeshCholek;
-var yeshLizaher = /יש ליזהר/
-var tzarichLizaher = /צריך ליזהר/
-var beMedinosElu = /במדינות אלו/
+var yeshLizaher = /יש ליזהר/g;
+var tzarichLizaher = /צריך ליזהר/g;
+var beMedinosElu = /במדינות אלו/g;
+var yeshLeHistapek = /יש להסתפק/g;
+var rambam = /רמב"ם/g;
+var rosh = /רא"ש/g;
+
+const query = (text, regex) => {(text.match(veYeshOmrimStart)|| []).length};
--- readme.md
+++ readme.md
@@ -2,29 +2,16 @@
 
 ## Goal
 To statistically analyze the text of Shulchan Aruch in light of the Klalim. See http://halachipedia.com/index.php?title=Shulchan_Aruch for a basic treatment of guidelines for reading Shulchan Aruch.
+
+Provides:
+* Raw statistics
+* Allows querying your own regex
+* Interactive queires - run a query and see all the resulting Simanim/Seifim
 ## Raw Statistics
 ### Shulchan Aruch
-
-    Stam veYesh Omrim - סתם ויש אומרים
-
-    Stam veYesh MiSheOmer - סתם ויש מי שאומר
-
-    Stam veYesh Mi SheCholek - סתם ויש מי שחולק
-
-    Yesh Omrim veYesh Omrim - יש אומרים ויש אומרים
-
-    Yesh Omrim veYesh Mi SheOmer - יש אומרים ויש מי שאומר
-
-    Yesh Omrim veYesh Mi SheCholek - יש אומרים ויש מי שחולק
-
-    Yesh Mi SheNoheg - יש מי שנוהג
-
-    Yesh Nohagim/n - יש נוהגים\ן
-
+#### Leshonot
 #### Rishonim Mentioned by Name
-Rambam
 
-Rosh
 ### Rama
 Hagahs
 
