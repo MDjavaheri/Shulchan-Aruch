8ba76db6973d4caba3ef23d47e20a576f232a522
MDjavaheri <michael.djavaheri@mail.yu.edu>
2018-06-01 06:29:12 +0300

    finish regexes, rename descriptions

--- queries.js
+++ queries.js
@@ -19,80 +19,104 @@ var cleanup = (data) => {
 var queries = [
     {
         description: 'Stam veYesh',
-        regex: ''
+        regex: /[^יש]* ויש /g
     },
     {
-        description: 'yeshOmrimVeYeshOmrim',
-        regex: ''
+        description: 'Yesh veYesh (beginning, all)',
+        regex: /^יש .* ויש/g
     },
     {
-        description: 'yeshOmrimVeYeshOmer',
-        regex: ''
+        description: 'Yesh Omrim veYesh Omrim',
+        regex: /^יש אומרים .* ויש אומרים/g
+    },
+    {
+        description: 'Yesh Omrim veYesh MiSheOmer',
+        regex: /^יש מי שאומר .* ויש אומרים/g
     },
     {
         description: 'yeshOmrimVeYeshCholek',
         regex: ''
     },
     {
-        description: 'veYeshOmrimStart',
+        description: 'VeYeshOmrim (beginning)',
         regex: /^ו(י"א|יש אומרים)/g
     },
     {
-        description: 'yeshYachidOpening',
+        description: 'Yesh Mi SheOmer (beginning)',
         regex: /^יש מי שאומר/g
     },
     {
-        description: 'veYeshOmerStart',
+        description: 'VeYesh Mi SheOmer (beginning)',
         regex: /^ו(יש מי שאומר)/g
     }, 
     {
-        description: 'yeshYachid',
+        description: 'Yesh Mi SheOmer',
         regex: /יש מי שאומר/g
     },
     {
-        description: 'yeshMattirOpening',
+        description: 'Yesh Mi SheMattir (beginning)',
         regex: /^יש מי שמתיר/g
     },
     {
-        description: 'yeshMattir',
+        description: 'Yesh Mi SheMattir',
         regex: /יש מי שמתיר/g
-    }
-    ,
+    },
     {
-        description: 'yeshNohegYachidOpening',
+        description: 'Yesh Mattirim',
+        regex: /יש מתירים/g
+    },
+    {
+        description: 'Yesh Mi SheNoheg (beginning)',
         regex: /^יש מי שנוהג/g
     },
     {
-        description: 'yeshNohegYachid',
+        description: 'Yesh Mi SheNoheg',
         regex: /יש מי שנוהג/g
     },
     {
-        description: 'yeshNohagimOpening',
+        description: 'Yesh Nogahim (beginning)',
         regex: /^יש נוהגי[םן]/g
     },
     {
-        description: 'yeshNohagim',
+        description: 'Yesh Nogahim',
         regex: /יש נוהגי[םן]/g
     },
     {
-        description: 'yeshCholkim',
+        description: 'Yesh Cholkim',
         regex: /יש חולקי[םן]/g
     },
     {
-        description: 'yeshCholek',
+        description: 'Yesh Mi SheCholek',
         regex:  /יש מי שחולק/g
     },
     {
-        description: 'stamVeYeshCholek',
-        regex: ''
+        description: 'Yesh Mi SheKatav',
+        regex:  /יש מי שכתב/g
     },
     {
-        description: 'tzarichLizaher',
+        description: 'Stam veYesh Mi SheOmer',
+        regex: /[^יש]* ויש מי שאומר/g
+    },
+    {
+        description: 'Stam veYesh Mi SheCholek',
+        regex: /[^יש]* ויש מי שחולק/g
+    },
+
+    {
+        description: 'Stam veYesh Cholkim',
+        regex: /[^יש]* ויש חולקים/g
+    },
+    {
+        description: 'Tzarich Lizaher',
         regex: /צריך ליזהר/g
     },
     {
         description: 'yeshLizaher',
         regex: /יש ליזהר/g
+    },
+    {
+        description: 'Tov',
+        regex: /טוב ל/g
     },
     {
         description: 'beMedinosElu',
@@ -109,6 +133,26 @@ var queries = [
     {
         description: 'rosh',
         regex: /רא"ש/g
+    },
+    {
+        description: 'Nachon',
+        regex: /נכון/g
+    },
+    {
+        description: 'Ashkenaz',
+        regex: /אשכנז/g
+    },
+    {
+        description: 'Sefarad',
+        regex: /ספרד/g
+    },
+    {
+        description: 'Tzorfat',
+        regex: /צרפת/g
+    },
+    {
+        description: 'Minhag',
+        regex: /מנהג/g
     }
 ];
 
@@ -128,12 +172,11 @@ var run = () => {
                         "count": matches.length
                     }
                     console.log(`${q.description}: ${matches.length}`);
-                    console.log(queries);
                 });
             });
         });
+        console.log(queries);
     });
-    console.log(queries);
-}
+};
 
 run();
